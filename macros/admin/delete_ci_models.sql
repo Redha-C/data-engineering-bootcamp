c01a2f9f8b28ca649f80fae05e0a60294fdcd9cc
Redha-C <c-redha@hotmail.fr>
2025-03-25 17:42:08 +0100

    modif macro to delete ci models according to BigQuery

--- macros/admin/delete_ci_models.sql
+++ macros/admin/delete_ci_models.sql
@@ -1,17 +1,28 @@
-{% macro delete_ci_models(dbt_pr_id, materialization) -%}
+{% macro delete_ci_models(dbt_pr_id, materialization) %}
+  {% set project_id = target.project %}
+  {% set dataset = target.schema %}
+  {% set table_type = 'BASE TABLE' if materialization == 'table' else 'VIEW' %}
 
-    {% call statement('get_pr_tables', fetch_result=True) %}
-        SHOW {{ materialization }}s FROM {{ schema }}
-        LIKE 'pr_{{ dbt_pr_id }}_{{ project_name }}*';
+  {% set query %}
+    SELECT table_schema, table_name
+    FROM `{{ project_id }}.{{ dataset }}.INFORMATION_SCHEMA.TABLES`
+    WHERE table_name LIKE 'pr_{{ dbt_pr_id }}_%'
+      AND table_type = '{{ table_type }}'
+  {% endset %}
 
+  {% call statement('get_ci_models', fetch_result=True) %}
+    {{ query }}
+  {% endcall %}
+
+  {% for row in load_result('get_ci_models')['data'] %}
+    {% set table_schema = row[0] %}
+    {% set table_name = row[1] %}
+
+    {% call statement('drop_ci_model_' ~ loop.index) %}
+      DROP {{ materialization.upper() }} IF EXISTS `{{ project_id }}.{{ table_schema }}.{{ table_name }}`
     {% endcall %}
 
-    {%- for to_delete in load_result('get_pr_tables')['data'] %}
-    {% call statement() -%}
-      {% do log(to_delete, info=true) %}
-      drop {{ materialization }} if exists {{ to_delete[0] }}.{{ to_delete[1] }} ;
-    {%- endcall %}
-  {%- endfor %}
+    {% do log("Dropped: " ~ table_name, info=true) %}
+  {% endfor %}
 
-
-{%- endmacro %}
+{% endmacro %}
