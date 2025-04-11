bfe457bddbc8511f0b7b322faa24e99799a2e7ab
Redha-C <c-redha@hotmail.fr>
2025-04-11 06:57:56 +0000

    add airflow folder with the dags folder and the requirements.txt

---
+++ airflow/dags/jaffle_shop_dag.py
@@ -0,0 +1,53 @@
+from airflow import DAG
+from airflow.providers.google.cloud.operators.kubernetes_engine import GKEStartPodOperator
+import pendulum
+from datetime import timedelta
+import os
+
+# Default arguments for the DAG
+default_args = {
+    "owner": "standupfree",
+    "depends_on_past": False,
+    "email_on_failure": True,
+    "email": ["standupfree.contact@gmail.com"],
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
+}
+
+# Define your Docker image and GKE settings
+PROJECT_ID = "ae-bootcamp-standupfree"
+REGION = "us"
+REPOSITORY = "dbt-images-prod"  # or dev/preprod depending on the DAG
+IMAGE_NAME = "dbt-jaffle-shop"
+DOCKER_IMAGE_TAG = "latest"  # or specify a version like "1.0.0"
+
+IMAGE_URI = f"{REGION}-docker.pkg.dev/{PROJECT_ID}/{REPOSITORY}/{IMAGE_NAME}:{DOCKER_IMAGE_TAG}"
+
+# Define the DAG
+with DAG(
+    dag_id="dbt_jaffle_shop_dag",
+    default_args=default_args,
+    description="Run dbt jaffle shop image on GKE",
+    schedule="@daily",  # Adjust as needed
+    start_date=pendulum.today('UTC').add(days=-1),
+    catchup=False,
+    tags=["dbt", "jaffle_shop" , "daily" , "ae_bootcamp", "standupfree"],
+) as dag:
+
+    run_dbt = GKEStartPodOperator(
+    		task_id="dbt_jaffle_shop_dag",
+		project_id="ae-bootcamp-standupfree",
+		service_account_name="k8s-dbt-job",
+    		location="us-central1",
+    		cluster_name="autopilot-cluster-1",
+   	 	namespace="default",
+		do_xcom_push=True,
+    		image="us-docker.pkg.dev/ae-bootcamp-standupfree/dbt-images-prod/dbt-jaffle-shop:latest",
+    		cmds=["dbt", "build", "--target", "prod"],
+    		name="dbt_jaffle_shop_job",
+		in_cluster=False,
+		on_finish_action="delete_pod",
+    )
+
+    run_dbt
+
